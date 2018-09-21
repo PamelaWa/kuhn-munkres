c6a42ad71b3efe4c987f5c34d7b36d7b4cf0cf1f
Patrick Sigourney <psigourney@gmail.com>
2018-09-21 14:53:34 -0500

    setup almost complete, starting on algo steps

--- src/KM.java
+++ src/KM.java
@@ -8,8 +8,9 @@ import java.util.LinkedList;
 public class KM {
 
     static class Vertex{
-        public char partite;
-        public Integer id;
+        public char partite;  //'x' or 'y'
+        public Integer id;    //node ID
+        public Integer label = 0;
         public LinkedList<Edge> edges = new LinkedList<Edge>();
 
         public Vertex(char partiteParam, int idParam){
@@ -18,49 +19,51 @@ public class KM {
         }
 
         public boolean equals(Object obj){
-            if(this == obj)
-                return true;
-            if(obj == null)
-                return false;
+            if(obj == this) return true;
+            if(obj == null) return false;
             if(obj instanceof Vertex){
                 Vertex vObj = (Vertex)obj;
                 if(this.partite == vObj.partite && this.id == vObj.id)
                     return true;
             }
             return false;
+        }
 
+        @Override
+        public String toString(){
+            return partite + Integer.toString(id) + "-label:" + Integer.toString(label);
         }
     }
 
     static class Edge{
-        public int x;
-        public int y;
+        public int xId;
+        public int yId;
         public int weight = 0;
 
         public Edge(int xParam, int yParam, int weightParam){
-            x = xParam;
-            y = yParam;
+            xId = xParam;
+            yId = yParam;
             weight = weightParam;
         }
 
         public boolean equals(Object obj){
-            if(this == obj)
-                return true;
-            if(obj == null)
-                return false;
+            if(obj == this) return true;
+            if(obj == null) return false;
             if(obj instanceof Edge){
                 Edge eObj = (Edge)obj;
-                if(this.x == eObj.x && this.y == eObj.y)
+                if(this.xId == eObj.xId && this.yId == eObj.yId)
                     return true;
             }
             return false;
-
         }
 
-
+        @Override
+        public String toString(){
+            return "(" + xId + "," + yId + "):W" + weight;
+        }
     }
 
-    public static LinkedList<Vertex> LoadInput(String inputFile){
+    public static LinkedList<Vertex> loadInput(String inputFile){
         LinkedList<Vertex> vertexList = new LinkedList<Vertex>();
         try{
             BufferedReader br = new BufferedReader(new FileReader(inputFile));
@@ -72,7 +75,7 @@ public class KM {
                 String line = br.readLine();
                 String[] arrLine = (line.split("\\s+"));
                 for(Integer j = 1; j <= n; j++){    //For each column - Create Edges
-                    Edge e = new Edge(i, j, Integer.valueOf(arrLine[0]));
+                    Edge e = new Edge(i, j, Integer.valueOf(arrLine[j-1]));
                     xv.edges.push(e);
                     yv.edges.push(e);
                 }
@@ -82,13 +85,73 @@ public class KM {
             }
         }catch (Exception e) {System.out.println("LoadInput() Exception!: " + e);}
         return vertexList;
-
     }
+
+    public static LinkedList<Edge> loadEdges(LinkedList<Vertex> vertexList){
+        LinkedList<Edge> edgeList = new LinkedList<Edge>();
+        for(Vertex v : vertexList){
+            for(Edge e : v.edges){
+                edgeList.add(e);
+            }
+        }
+        return edgeList;
+    }
+
+    public static void initializeXlabels(LinkedList<Vertex> vertexList){
+        for(Vertex v :  vertexList){
+            if(v.partite == 'y') continue;  //X nodes only
+            int maxWeight = 0;
+            for(Edge e : v.edges){
+                if(e.weight > maxWeight) maxWeight = e.weight;
+            }
+            v.label = maxWeight;
+        }
+    }
+
+    public static LinkedList<Edge> findTightEdges(LinkedList<Vertex> vertexList){
+        LinkedList<Edge> equalityList = new LinkedList<Edge>();
+        for(Vertex v : vertexList){
+            if(v.partite == 'y') continue; //We only want the 'x' nodes.
+            for(Edge e : v.edges){
+                int yLabel = getVertex(vertexList, 'y', e.yId).label;
+                int xLabel = v.label;
+                int edgeWeight = e.weight;
+                if(xLabel + yLabel == edgeWeight){
+                    equalityList.add(e);
+                }
+            }
+        }
+        return equalityList;
+    }
+
+
+    //TODO: might delete this... equalityGraph should be the tight edges, not the vertices.
+    public static LinkedList<Vertex> findEquality(LinkedList<Vertex> vertexList){
+        LinkedList<Vertex> equalityGraph = new LinkedList<Vertex>();
+        //If the tight edge vertices aren't already in equalityGraph, add them.
+        for(Edge e : findTightEdges(vertexList)){
+            if(getVertex(equalityGraph, 'x', e.xId) == null)
+                equalityGraph.add(getVertex(vertexList, 'x', e.xId));
+            if(getVertex(equalityGraph, 'y', e.yId) == null)
+                equalityGraph.add(getVertex(vertexList, 'y', e.yId));
+        }
+        return equalityGraph;
+    }
+
 
     public static Vertex getVertex(LinkedList<Vertex> vertexList, char partiteParam, int idParam){
         for(Vertex v : vertexList){
             if(v.partite == partiteParam && v.id == idParam)
                 return v;
+        }
+        return null;
+    }
+
+
+    public static Edge getEdge(LinkedList<Edge> edgeList, int xParam, int yParam){
+        for(Edge e : edgeList){
+            if(e.xId == xParam && e.yId == yParam)
+                return e;
         }
         return null;
     }
@@ -101,11 +164,39 @@ public class KM {
         }
 
         long startTime = System.nanoTime();
-        LinkedList<Vertex> vertexList = LoadInput(args[0]);
-        LinkedList<Edge> matchedEdges = new LinkedList<Edge>();
-        final int n = vertexList.size() / 2;
+        //One Time Functions:
+        LinkedList<Vertex> vertexList = loadInput(args[0]);         //List of all vertices
+        LinkedList<Edge> edgeList = loadEdges(vertexList);          //List of all edges
+        initializeXlabels(vertexList);                              //Initialize x vertex labels to weight of max edge
+
+
+        //Functions that will be called multiple times:
+        LinkedList<Edge> equalityGraph = findTightEdges(vertexList); //List of tight edges
+
+        LinkedList<Edge> matchedEdges = new LinkedList<Edge>();     // "M set of matched edges"
+        LinkedList<Vertex> S = new LinkedList<Vertex>();            // "S set of x vertices"
+        LinkedList<Vertex> T = new LinkedList<Vertex>();            // "T set of y vertices"
+
+        LinkedList<Edge> augPath = new LinkedList<Edge>();          //stores an augmenting path
+        int alpha = 0;                                              //alpha for label updates
+        final int n = vertexList.size() / 2;                        // "n" (perfect matching)
+
+        //TODO: Create function to find/return augmenting path:  augPath = findAugPath(equalityList, matchedEdges);
+
+        //TODO: Create function to return alpha value:  alpha = findAlpha(vertexList, edgeList
+
+
+        Vertex x1 = getVertex(vertexList, 'x', 1);
+        Vertex y2 = getVertex(vertexList, 'y', 2);
+        System.out.println("x1.edges: " + x1.edges);
+        System.out.println("y2.edges: " + y2.edges);
+
+        System.out.println("equalityGraph:" + equalityGraph);
+
         while(matchedEdges.size() < n){
             //TODO: Do the algorithm
+
+            break;
         }
 
 
